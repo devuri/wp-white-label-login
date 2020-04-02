c7ae1b29db32107991123b5d921c94190c030ce0
devuri <devuriko@gmail.com>
2020-04-02 16:09:45 -0500

    do some cleanup after user uninstalls the plugin

---
+++ uninstall.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ *  Uninstall stuff.
+ *  do some cleanup after user uninstalls the plugin
+ *  ----------------------------------------------------------------------------
+ *  -remove stuff
+ * ----------------------------------------------------------------------------
+ * @category  	Plugin
+ * @copyright 	Copyright © 2020 Uriel Wilson.
+ * @package   	WhiteLabelLogin
+ * @author    	Uriel Wilson
+ * @link      	https://switchwebdev.com
+ * ----------------------------------------------------------------------------
+ */
+
+	# deny direct access
+  if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	  die;
+  }
+
+  # delete settings in the options table.
+	delete_option('wpwll_logo_url');
+	delete_option('wpwll_background_url');
+	delete_option('wpwll_align');
+	delete_option('wpwll_custom_css');
