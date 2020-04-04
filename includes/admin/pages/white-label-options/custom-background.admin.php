530d4bfb3a5fbde3f9ed59694ca67484825e45cc
devuri <devuriko@gmail.com>
2020-04-04 03:15:36 -0500

    remove the image

--- includes/admin/pages/white-label-options/custom-background.admin.php
+++ includes/admin/pages/white-label-options/custom-background.admin.php
@@ -1,14 +1,7 @@
-<?php
-
-	// options
-		$get_background_image = $this->plugin()->option('background');
-
-?><div class"wll-status">
+<div class"wll-status">
 	<strong>Get Awesome free Photos:</strong>
 	<a target="_blank" href="https://pixabay.com/photos/search/">pixabay.com</a>
 </div>
-<hr/>
-<img width="600"src="<?php echo wp_get_attachment_url($get_background_image); ?>" alt="">
 <hr/>
 <div id="frmwrap" >
 	<?php echo $this->plugin()->customizer_button('Change Background'); ?>
