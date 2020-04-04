8be5f08b8998653626960380af03f7dba7c36cb3
devuri <devuriko@gmail.com>
2020-04-04 03:12:13 -0500

    Customizer layout setting and control

---
+++ includes/customize/settings/layout.php
@@ -0,0 +1,24 @@
+<?php
+
+
+$wp_customize->add_setting( 'wpwll_options[form_layout]',
+	array(
+		'type' 							=> 'option',
+		'capability' 				=> 'manage_options',
+		'default' 					=> 'center',
+		'transport' 				=> 'postMessage', // or 'refresh'
+		'sanitize_callback' => 'sanitize_title',
+	) );
+
+$wp_customize->add_control( 'wpwll_options[form_layout]',
+	array(
+	  'type' => 'radio',
+	  'section' => 'white_label_layout',
+	  'label' => __( 'Custom Form Alignment' ),
+	  'description' => __( 'Choose how to align the login form.' ),
+	  'choices' => array(
+	    'left' 		=> __( 'Left' ),
+	    'right' 	=> __( 'Right' ),
+	    'center' => __( 'Center' ),
+	  ),
+	) );
