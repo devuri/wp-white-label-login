6720893521e33668146665d6f883c0bc9098be40
devuri <devuriko@gmail.com>
2020-04-04 03:14:46 -0500

    remove the logo

--- includes/admin/pages/white-label-options/white-label-options.admin.php
+++ includes/admin/pages/white-label-options/white-label-options.admin.php
@@ -1,15 +1,13 @@
-<?php
-
-	// options
-		$get_logo 			= $this->plugin()->option('logo');
-
-?><div class"wll-status">
+<div class"wll-status">
 	<strong>Get Awesome free Photos:</strong> <a target="_blank" href="https://pixabay.com/photos/search/">pixabay.com</a> .
+	 <br/>
 	 Give us a rating on <a target="_blank" href="https://wordpress.org/plugins/wp-white-label-login/">WordPress.org</a>.
 </div>
 <hr/>
-<img width="120"src="<?php echo wp_get_attachment_url($get_logo); ?>" alt="">
-<hr/>
+
 <div id="frmwrap" >
-	<?php echo $this->plugin()->customizer_button('Change Logo'); ?>
+	<?php echo $this->plugin()->customizer_button('Change Logo');
+
+	//	print_r($this->plugin()->option('wpwll_options'));
+	?>
 </div><!--frmwrap-->
