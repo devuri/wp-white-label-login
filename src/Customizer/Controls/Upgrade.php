e67c487130676bb209ffd3ae573a614fad61fa42
devuri <devuriko@gmail.com>
2020-04-05 19:28:40 -0500

    moved updagrade control

---
+++ src/Customizer/Controls/Upgrade.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * Upgrade Control
+ */
+if ( class_exists( 'WP_Customize_Control' ) ) :
+
+	/**
+	 * Displays the upgrade message .
+	 */
+	class WllCustomizeProControl extends WP_Customize_Control {
+		/**
+		 * Render Control
+		 */
+		public function render_content() {
+			?>
+
+			<div class="upgrade-pro-version">
+
+				<span class="customize-control-title"><?php esc_html_e( 'Pro Version Add-on', 'whitelabel-login' ); ?></span>
+
+				<span class="textfield">
+					<?php printf( esc_html__( 'Purchase the %s Add-on to get additional features and advanced customization options.', 'whitelabel-login' ), 'White Label Login Pro' ); ?>
+				</span>
+
+				<p>
+					<a href="<?php echo esc_url( __( 'https://whitelabelwp.net/add-ons/whitelabel-login-pro/', 'whitelabel-login' ) ); ?>" target="_blank" class="button button-secondary">
+						<?php printf( esc_html__( 'Learn more about %s', 'whitelabel-login' ), 'White Label Login Pro' ); ?>
+					</a>
+				</p>
+
+			</div>
+
+			<?php
+		}
+	}
+
+endif;
