eb7ef2891a10220f7e52697752ccfce32ed2dc1e
devuri <devuriko@gmail.com>
2020-04-12 13:57:56 -0500

    new button option

---
+++ src/Customize/Settings/button.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+* Background Color
+* @var [type]
+*/
+$this->customizer()->add_setting( 'wpwll_options[button_background_color]',
+array(
+	'type' 							=> 'option',
+	'capability' 				=> 'manage_options',
+	'default' 					=> '#007cba',
+	'transport' 				=> $this->preview_type,
+	'sanitize_callback' => 'sanitize_hex_color',
+) );
+
+$this->customizer()->add_control(
+ new WP_Customize_Color_Control(
+	$this->customizer(), 'wpwll_options[button_background_color]',
+	 array(
+		 'label' => __( 'Background Color' ),
+		 'description' => __( 'Select a color' ),
+		 'section' => 'white_label_button', // Add a default or your own section
+		)
+) );
+
+
+/**
+ * Text Color
+ * @var [type]
+ */
+$this->customizer()->add_setting( 'wpwll_options[button_text_color]',
+	array(
+		'type' 							=> 'option',
+		'capability' 				=> 'manage_options',
+		'default' 					=> '#ffffff',
+		'transport' 				=> $this->preview_type,
+		'sanitize_callback' => 'sanitize_hex_color',
+	) );
+
+$this->customizer()->add_control(
+	 new WP_Customize_Color_Control(
+		$this->customizer(), 'wpwll_options[button_text_color]',
+		 array(
+			 'label' => __( 'Text Color' ),
+			 'description' => __( 'Select a color' ),
+			 'section' => 'white_label_button', // Add a default or your own section
+			)
+	) );
