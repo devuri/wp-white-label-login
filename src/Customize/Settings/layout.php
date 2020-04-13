dd9d9c08c997115e59fed05eb5a4e07f9be3d9ce
devuri <devuriko@gmail.com>
2020-04-13 07:24:10 -0500

    fix to work with new section name prefix

--- src/Customize/Settings/layout.php
+++ src/Customize/Settings/layout.php
@@ -6,14 +6,14 @@ $this->customizer()->add_setting( 'wpwll_options[form_layout]',
 		'type' 							=> 'option',
 		'capability' 				=> 'manage_options',
 		'default' 					=> 'center',
-		'transport' 				=> $this->preview_type, 
+		'transport' 				=> $this->preview_type,
 		'sanitize_callback' => 'sanitize_title',
 	) );
 
 $this->customizer()->add_control( 'wpwll_options[form_layout]',
 	array(
 	  'type' => 'radio',
-	  'section' => 'white_label_layout',
+	  'section' => 'whitelabel_section_layout',
 	  'label' => __( 'Custom Form Alignment' ),
 	  'description' => __( 'Choose how to align the login form.' ),
 	  'choices' => array(
