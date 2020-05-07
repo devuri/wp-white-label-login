de9cef6356c113c54cebab876ee6814e2f50de4f
devuri <devuriko@gmail.com>
2020-05-06 23:41:17 -0500

    minor fix to the settings order

--- src/Customize/Settings/footer.php
+++ src/Customize/Settings/footer.php
@@ -1,67 +1,4 @@
 <?php
-
-/**
- * footer_text
- * @var [type]
- */
-$this->customizer()->add_setting( 'wpwll_options[footer_text]',
-	array(
-		'type' => 'option',
-		'capability' => 'manage_options',
-		'default' => '...',
-		'transport' => $this->preview_type,
-		'sanitize_callback' => 'sanitize_textarea_field'
-	) );
-
-$this->customizer()->add_control('wpwll_options[footer_text]', array(
-		'label'   => 'Footer Text',
-		'description' 	=> __( 'Add Text to the footer.' ),
-		'section' => 'whitelabel_section_footer',
-		'type'    => 'textarea',
-));
-
-/**
- * copyright_text
- * @var [type]
- */
-$this->customizer()->add_setting( 'wpwll_options[copyright_text]',
-	array(
-		'type' => 'option',
-		'capability' => 'manage_options',
-		'default' => 'All Rights Reserved.',
-		'transport' => $this->preview_type,
-		'sanitize_callback' => 'sanitize_text_field'
-	) );
-
-$this->customizer()->add_control('wpwll_options[copyright_text]', array(
-		'label'   => 'Change Copyright Text',
-		'section' => 'whitelabel_section_footer',
-		'type'    => 'text',
-));
-
-/**
- * Footer Color
- * @var [type]
- */
-$this->customizer()->add_setting( 'wpwll_options[footer_text_color]',
-	array(
-		'type' 							=> 'option',
-		'capability' 				=> 'manage_options',
-		'default' 					=> '#747474',
-		'transport' 				=> $this->preview_type,
-		'sanitize_callback' => 'sanitize_hex_color',
-	) );
-
-$this->customizer()->add_control(
-	 new WP_Customize_Color_Control(
-		$this->customizer(), 'wpwll_options[footer_text_color]',
-		 array(
-			 'label' => __( 'Text Color' ),
-			 'description' => __( 'Select a color' ),
-			 'section' => 'whitelabel_section_footer',
-			)
-	) );
-
 /**
  * Footer Alignment
  * @var [type]
@@ -87,3 +24,64 @@ $this->customizer()->add_control( 'wpwll_options[footer_alignment]',
 				'right' 		=> __('Right')
 			),
 	) );
+
+/**
+ * footer_text
+ * @var [type]
+ */
+$this->customizer()->add_setting( 'wpwll_options[footer_text]',
+	array(
+		'type' => 'option',
+		'capability' => 'manage_options',
+		'default' => '...',
+		'transport' => $this->preview_type,
+		'sanitize_callback' => 'sanitize_textarea_field'
+	) );
+
+$this->customizer()->add_control('wpwll_options[footer_text]', array(
+		'label'   => 'Footer Text',
+		'description' 	=> __( 'Add Text to the footer.' ),
+		'section' => 'whitelabel_section_footer',
+		'type'    => 'textarea',
+));
+/**
+ * Footer Text Color
+ * @var [type]
+ */
+$this->customizer()->add_setting( 'wpwll_options[footer_text_color]',
+	array(
+		'type' 							=> 'option',
+		'capability' 				=> 'manage_options',
+		'default' 					=> '#747474',
+		'transport' 				=> $this->preview_type,
+		'sanitize_callback' => 'sanitize_hex_color',
+	) );
+
+$this->customizer()->add_control(
+	 new WP_Customize_Color_Control(
+		$this->customizer(), 'wpwll_options[footer_text_color]',
+		 array(
+			 'label' => __( 'Text Color' ),
+			 'description' => __( 'Select a color' ),
+			 'section' => 'whitelabel_section_footer',
+			)
+	) );
+
+/**
+ * copyright_text
+ * @var [type]
+ */
+$this->customizer()->add_setting( 'wpwll_options[copyright_text]',
+	array(
+		'type' => 'option',
+		'capability' => 'manage_options',
+		'default' => 'All Rights Reserved.',
+		'transport' => $this->preview_type,
+		'sanitize_callback' => 'sanitize_text_field'
+	) );
+
+$this->customizer()->add_control('wpwll_options[copyright_text]', array(
+		'label'   => 'Copyright Text',
+		'section' => 'whitelabel_section_footer',
+		'type'    => 'text',
+));
