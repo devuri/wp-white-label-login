93e554d1df2c34081ebfe8e192feb93d61784da3
devuri <devuriko@gmail.com>
2020-05-16 23:05:34 -0500

    v5.6.0

--- white-label-login.php
+++ white-label-login.php
@@ -5,7 +5,7 @@
  * Description: White Label Login, Custom Login Page, Registration and Lost Password Page, Activate it and forget it...
  * Author:      SwitchWebdev.com
  * Author URI:  https://switchwebdev.com
- * Version:     5.5.1
+ * Version:     5.6.0
  * License:     GPLv2
  * License URI: https://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain: wp-white-label-login
@@ -36,69 +36,69 @@
  *  ----------------------------------------------------------------------------
  */
 
-  # deny direct access
-    if ( ! defined( 'WPINC' ) ) {
-      die;
-    }
+	# deny direct access
+		if ( ! defined( 'WPINC' ) ) {
+			die;
+		}
 
-  # plugin directory
-	  define("WPWLL_VERSION", '5.5.1');
+	# plugin directory
+		define("WPWLL_VERSION", '5.6.0');
 
-  # plugin directory
-    define("WPWLL_DIR", dirname(__FILE__));
+	# plugin directory
+		define("WPWLL_DIR", dirname(__FILE__));
 
-  # plugin url
-    define("WPWLL_URL", plugins_url( "/",__FILE__ ));
+	# plugin url
+		define("WPWLL_URL", plugins_url( "/",__FILE__ ));
 #  -----------------------------------------------------------------------------
 
 	//Activate
 	register_activation_hook( __FILE__, 'wpwll_activation' );
 	function wpwll_activation() {
-	   /**
-      * $wpwlldefauts load some defauts
-      * @var array
-      */
-	   $wpwlldefauts = array(
-       'form_layout' => 'center',
-       // header
-       'header_title' => get_bloginfo('name'),
-       'header_description' => get_bloginfo('description'),
-       'header_text_color' => '#737373',
-       'header_background_color' => '#ffffff',
-       'header_alignment' => 'center',
-       //logo
-       'logo_display' => 'none',
-       'logo_position' => 'center',
-       //login
-       'login_text_color' => '#444444',
-       'login_container_color' => '#ffffff',
-       //form
-       'login_form_color' => '#ffffff',
-       //button
-       'button_text_color' => '#ffffff',
-       'button_background_color' => '#007cba',
-       //links
-       'link_color' => '#474748',
-       //background
-       'background_color' => '#ffffff',
-       'background_attachment' => 'fixed',
-       'background_repeat' => 'no-repeat',
-       'background_position' => 'bottom',
-       //menu
-       'footer_nav' => false,
-       'footer_nav_backgorund' => '#dadada',
-       'footer_nav_alignment' => 'center',
-       // footer
-       'footer_text' => '...',
-       'copyright_text' => 'All Rights Reserved',
-       'footer_text_color' => '#747474',
-       'footer_alignment' => 'center',
-     );
+		 /**
+			* $wpwlldefauts load some defauts
+			* @var array
+			*/
+		 $wpwlldefauts = array(
+			 'form_layout' => 'center',
+			 // header
+			 'header_title' => get_bloginfo('name'),
+			 'header_description' => get_bloginfo('description'),
+			 'header_text_color' => '#737373',
+			 'header_background_color' => '#ffffff',
+			 'header_alignment' => 'center',
+			 //logo
+			 'logo_display' => 'none',
+			 'logo_position' => 'center',
+			 //login
+			 'login_text_color' => '#444444',
+			 'login_container_color' => '#ffffff',
+			 //form
+			 'login_form_color' => '#ffffff',
+			 //button
+			 'button_text_color' => '#ffffff',
+			 'button_background_color' => '#007cba',
+			 //links
+			 'link_color' => '#474748',
+			 //background
+			 'background_color' => '#ffffff',
+			 'background_attachment' => 'fixed',
+			 'background_repeat' => 'no-repeat',
+			 'background_position' => 'bottom',
+			 //menu
+			 'footer_nav' => false,
+			 'footer_nav_backgorund' => '#dadada',
+			 'footer_nav_alignment' => 'center',
+			 // footer
+			 'footer_text' => '...',
+			 'copyright_text' => 'All Rights Reserved',
+			 'footer_text_color' => '#747474',
+			 'footer_alignment' => 'center',
+		 );
 		update_option('wpwll_options', $wpwlldefauts );
 		update_option('wpwll_background', '0' );
 		update_option('wpwll_logo', '0' );
 		update_option('wpwll_custom_css', '' );
-    // TODO redirect to a welcome admin page
+		// TODO redirect to a welcome admin page
 	}
 
 	// Deactivate
@@ -109,26 +109,26 @@
 		// moved to uninstall.php
 	}
 
-  /**
-   * require_once // Load the WhiteLabel class.
-   * @var [type]
-   */
-  require_once WPWLL_DIR . '/src/WhiteLabel.php';
+	/**
+	 * require_once // Load the WhiteLabel class.
+	 * @var [type]
+	 */
+	require_once WPWLL_DIR . '/src/WhiteLabel.php';
 
 
-  if (! function_exists('wpwhitelabel')) {
-    /**
-     * wpwhitelabel()
-     *
-     * we can turn the login style on and off here
-     * @param  boolean $enable
-     * @return object
-     */
-    function wpwhitelabel( $enabled = true ) {
-      // new up wll object
-      return WPWhiteLabel\WhiteLabel::instance($enabled);
-    }
-  }
+	if (! function_exists('wpwhitelabel')) {
+		/**
+		 * wpwhitelabel()
+		 *
+		 * we can turn the login style on and off here
+		 * @param  boolean $enable
+		 * @return object
+		 */
+		function wpwhitelabel( $enabled = true ) {
+			// new up wll object
+			return WPWhiteLabel\WhiteLabel::instance($enabled);
+		}
+	}
 // initiate --------------------------------------------------------
-  wpwhitelabel();
+	wpwhitelabel();
 // initiate --------------------------------------------------------
