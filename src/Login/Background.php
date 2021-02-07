06484317e67172df6657274df2aed08e9019e1ae
devuri <devuriko@gmail.com>
2021-02-07 09:52:46 -0500

    phpcs fix

--- src/Login/Background.php
+++ src/Login/Background.php
@@ -14,7 +14,7 @@ class LoginBackground {
 	 * @link https://developer.wordpress.org/reference/functions/wp_get_attachment_url/
 	 */
 	protected static function background(){
-		$background_img = wp_get_attachment_url(wpwhitelabel()->option('background_image'));
+		$background_img = wp_get_attachment_url(wpwhitelabel()->option( 'background_image' ));
 		echo $background_img;
 	}
 
@@ -27,12 +27,12 @@ class LoginBackground {
 	protected static function css() {
 		?><style type="text/css">
 			body {
-				background-color: <?php echo wpwhitelabel()->setting('background_color'); ?>;
+				background-color: <?php echo wpwhitelabel()->setting( 'background_color' ); ?>;
 				background-image: url(<?php self::background(); ?>);
-				background-attachment: <?php echo wpwhitelabel()->setting('background_attachment'); ?>;
-				background-size: <?php echo wpwhitelabel()->setting('background_size'); ?>;
-				background-repeat: <?php echo wpwhitelabel()->setting('background_repeat'); ?>;
-				background-position: <?php echo wpwhitelabel()->setting('background_position'); ?>;
+				background-attachment: <?php echo wpwhitelabel()->setting( 'background_attachment' ); ?>;
+				background-size: <?php echo wpwhitelabel()->setting( 'background_size' ); ?>;
+				background-repeat: <?php echo wpwhitelabel()->setting( 'background_repeat' ); ?>;
+				background-position: <?php echo wpwhitelabel()->setting( 'background_position' ); ?>;
 			}
 			</style><?php
 	}
