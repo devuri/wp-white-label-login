da8b02bc67c3691aad718456d36a79821a592802
devuri <devuri@users.noreply.github.com>
2024-01-16 09:22:31 -0500

    feat: update plugin info twice daily.

---
+++ src/EasyWhiteLabel/DailyTask.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * This file is part of the Easy White Label WordPress PLugin.
+ *
+ * (c) Uriel Wilson <hello@urielwilson.com>
+ *
+ * Please see the LICENSE file that was distributed with this source code
+ * for full copyright and license information.
+ */
+
+namespace EasyWhiteLabel;
+
+use EasyWhiteLabel\Traits\Singleton;
+
+class DailyTask {
+
+	use Singleton;
+
+	protected const PLUGINS_DATA_TRANSIENT = 'ewl_plugins_data';
+
+    public function scheduled() {
+        if ( ! wp_next_scheduled( 'ewl_update_plugins_list' ) ) {
+            wp_schedule_event( time(), 'twicedaily', 'ewl_update_plugins_list' );
+        }
+
+        add_action( 'ewl_update_plugins_list', array( $this, 'execute_task' ) );
+    }
+
+    public function execute_task()
+	{
+        $this->get_plugins_data( Plugin::get_plugins() );
+    }
+
+	protected function get_plugins_data( array $plugins )
+    {
+        $api_data = Transient::get( self::PLUGINS_DATA_TRANSIENT );
+
+        if ( ! $api_data ) {
+            $plugin_info = [];
+
+            foreach ( $plugins as $plugin ) {
+                $plugin_info[ $plugin ] = plugins_api(
+                    'plugin_information',
+                    [
+                        'slug'   => sanitize_file_name( $plugin ),
+                        // 'slug' => sanitize_file_name($plugin['slug']),
+                        'fields' => [
+                            'short_description' => true,
+                            'sections'          => false,
+                            'requires'          => false,
+                            'downloaded'        => true,
+                            'last_updated'      => false,
+                            'added'             => false,
+                            'tags'              => false,
+                            'compatibility'     => false,
+                            'homepage'          => false,
+                            'donate_link'       => false,
+                            'icons'             => true,
+                            'banners'           => true,
+                        ],
+                    ]
+                );
+            }// end foreach
+
+            Transient::set( self::PLUGINS_DATA_TRANSIENT, $plugin_info );
+        }// end if
+    }
+
+    public function deactivate() {
+        $timestamp = wp_next_scheduled( 'ewl_update_plugins_list' );
+        wp_unschedule_event( $timestamp, 'ewl_update_plugins_list' );
+    }
+}
--- src/EasyWhiteLabel/Plugin.php
+++ src/EasyWhiteLabel/Plugin.php
@@ -27,6 +27,27 @@ class Plugin
     public $enabled;
     public $customizer;
 
+	public static function get_plugins(): array
+	{
+		return [
+		    'application-passwords-manager',
+		    'disable-dashboard-widgets',
+		    'wp-auto-updates',
+		    'membership-lock',
+		    'iceyi-members-only',
+		    'sim-clickable-links',
+		    'better-search-replace',
+		    'disable-comments',
+		    'wp-seopress',
+		    'login-recaptcha',
+		    'sucuri-scanner',
+		    'wpforms-lite',
+		    'wp-mail-smtp',
+		    'wp-dbmanager',
+		    'rest-api-featured-image',
+		];
+	}
+
     /**
      * Add Shortcode [wpoption opt="myoption"].
      *
