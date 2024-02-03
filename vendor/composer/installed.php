a6d53d34b1df44ce7a3b026f050fa3a107023bd8
Uriel <devuri@users.noreply.github.com>
2024-02-03 01:56:52 -0500

    Add files via upload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'devuri/wp-easy-white-label',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '2910647d44df777a021178b525f82ce74558cb14',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'devuri/wp-easy-white-label' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '2910647d44df777a021178b525f82ce74558cb14',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
